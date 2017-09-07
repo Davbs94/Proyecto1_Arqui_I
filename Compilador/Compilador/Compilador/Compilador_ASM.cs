f939e1a2c4bb949453346b4dfc612d6f85c1127e
davbs94 <davbs94@gmail.com>
2017-09-07 16:09:16 -0600

    Se inicia con el diseno del compilador

---
+++ Compilador/Compilador/Compilador/Compilador_ASM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+    public class Compilador_ASM
+    {
+        private string _path;
+
+        public Compilador_ASM()
+        {
+           
+        }
+
+        public void set_path(string path)
+        {
+            _path = path;
+        }
+
+        public void lexico()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@_path);
+            Console.WriteLine(lines[0]);
+        }
+    }
+
---
+++ Compilador/Compilador/Compilador/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Compilador
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+        private Compilador_ASM comp = new Compilador_ASM();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            comp.set_path(filepath);
+            comp.lexico();
+            label2.Text = "File Compiled!";
+
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*.*";
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                filepath = choofdlog.FileName;
+                string[] arrAllFiles = choofdlog.FileNames; //used when Multiselect = true  
+                label1.Text = filepath;
+               
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
