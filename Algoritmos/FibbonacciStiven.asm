720086c642692a0d5263dff89c80d2205c6aaba8
Stiven Sanchez <stsanchez92@gmail.com>
2017-10-14 18:59:10 -0700

    fibonnaci

---
+++ Algoritmos/FibbonacciStiven.asm
@@ -0,0 +1,13 @@
+addi R1, R1 (0) #int a = 0;
+addi R2, R2 (1) #int b = 1;
+addi R3, R3 (0) # i = 0
+addi R4, R4 (5) # Se define "n", numero de iteraciones
+
+Loop:
+	beq   R4, R3, fin
+	addi  R5, R1, (0)
+	addi  R1, R2, (0)
+	add   R2, R5, R2
+	addi  R3, R3 (1)
+	j Loop
+fin:
